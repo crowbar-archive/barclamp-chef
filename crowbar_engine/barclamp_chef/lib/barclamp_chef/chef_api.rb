5f056a20db195c2ad5c4929aa79778bc3259553a
aabes <a_abes@yahoo.com>
2013-02-25 18:14:31 -0500

    move chef stuff out of barclamp to lib dir

--- crowbar_engine/barclamp_chef/lib/barclamp_chef/chef_api.rb
+++ crowbar_engine/barclamp_chef/lib/barclamp_chef/chef_api.rb
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,45 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#
 
+module BarclampChef
 
-require File.join(File.dirname(__FILE__),"monkeypatch","rest")
+  class ChefAPI
 
-module BarclampChef 
-
-  class Jig < Jig
-  
-    has_one :jig_chef_conn_info, :dependent => :destroy
 =begin
     Prepare the chef objects for use, by injecting the appropriate authentication 
     info from the DB.
 =end
-    def prepare_chef_api
-      conn_info = self.jig_chef_conn_info
+    def prepare_chef_api(conn_info)
       logger.info("No Chef connection info") and return unless conn_info
       Chef::Config.node_name = conn_info.client_name
       Chef::Config.chef_server_url = conn_info.url
       ReplacementAuthMod.replace_authenticator(conn_info.client_name, conn_info.key)
     end
 
-  
-    def create_event(config)
-      evt = JigEvent.create(:type=>"JigEvent", :proposal_confing =>config, 
-        :jig => self, :status => JigEvent::EVT_PENDING, :name=>"apply_#{config.id}")
-      evt
-    end
-  
-    def create_run_for(evt, nr,order)
-      run = JigRunChef.create(:type=> "JigRunChef", :jig_event => evt, 
-        :node_role => nr, :order=>order, :status => JigRun::RUN_PENDING, 
-        :name=>"run_#{evt.id}_#{nr.id}_#{order}")
-      run
-    end
+  end
 
-private
-
-    def node(name)
+   def node(name)
      begin 
        chef_init
        super.node name
@@ -60,6 +40,8 @@ private
        return nil
      end
     end
+
+
     def data(bag_item)
      begin 
        chef_init
@@ -103,6 +85,4 @@ private
      end
     end
 
-    
-  end # class
-end # module
+end
