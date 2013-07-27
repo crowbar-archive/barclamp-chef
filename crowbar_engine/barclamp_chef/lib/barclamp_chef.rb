0997bd6f3f1c2362e0bae8201cdc70ccec92deec
Victor Lowther <victor.lowther@gmail.com>
2013-07-27 11:30:42 -0500

    The Chef jig is alive, if not fully operational.
      * Moved kernel-panic cookbook from the Chef barclamp to here to
        satisfy deployer-client dependencies
      * Re-added dependencies on the Chef gems, as it is much easier to
        mess with all the Chef things we need to mess with that way.  We
        currently require chef 11.6.0, as it is much easier to inject our
        authentication tokens that way.
      * The chef jig creates Chef node credentials at point of need.
      * The chef jig uploads cookbooks, data bags, and roles at point of
        first use.  This eliminates the need for any special casing in
        barclamp_install to determine whether or not to try to upload
        anything to the Chef server.
      * inject_connection.rake is gone, since the default chef jig is
        created at barclamp import time now.

--- crowbar_engine/barclamp_chef/lib/barclamp_chef.rb
+++ crowbar_engine/barclamp_chef/lib/barclamp_chef.rb
@@ -17,9 +17,11 @@ module BarclampChef
       ].each {|k|ENV.delete(k)}
       # Sigh, knife needs EDITOR
       ENV["EDITOR"] ||= "true"
+      # Clean up PATH as well
+      ENV["PATH"] = ENV["PATH"].split(":").reject{|part| part =~ /crowbar_framework/}.join(":")
       exit!(system("knife #{args}"))
     end
-    Process.waitpid2(pid)[1].exitstatus
+    Process.waitpid2(pid)[1].exitstatus == 0
   end
 
   # Add role-specific overrides.
@@ -28,15 +30,12 @@ module BarclampChef
     # chef-server role needs an on_active helper
     module ChefServer
       def on_active(nr)
-        unless BarclampChef::Jig.exists?(:name => "chef")
-          j = BarclampChef::Jig.create(:name => "chef")
-          j.description = "Chef Jig"
-          j.order = 100
-          j.server = "http://#{nr.node.name}:4000"
-          j.client_name = "crowbar"
-          j.key = "/home/crowbar/.chef/crowbar.pem"
-          j.save!
-        end
+        j = BarclampChef::Jig.where(:name => "chef").first
+        j.server = "http://#{nr.node.name}:4000"
+        j.client_name = "crowbar"
+        j.active = true
+        j.key = "/home/crowbar/.chef/crowbar.pem"
+        j.save!
       end
 
       def on_error(nr)
@@ -51,16 +50,13 @@ module BarclampChef
           d = nr.data
           clientinfo = (d["chefjig"]["client"] || {} rescue {})
           return if clientinfo["key"]
-          clientinfo["name"]=nr.node.name
-          Dir.mktmpdir do |tempdir|
-            FileUtils.cd(tempdir) do
-              unless BarclampChef.knife("client create '#{nr.node.name}' -f ./client.pem")
-                raise "Cannot create Chef Client for #{nr.node.name}"
-              end
-              clientinfo["key"] = IO.read("client.pem")
-            end
-          end
-          d["chefjig"] ||= {}
+          chefjig = Jig.where(:name => "chef").first
+          raise "Cannot load Chef Jig" unless chefjig
+          chef_node, chef_role, chef_client = chefjig.create_node(nr.node)
+          raise "COuld not create chef client!" unless chef_client["private_key"]
+          clientinfo["key"] = chef_client["private_key"]
+          clientinfo["name"] = nr.node.name
+          d["chefjig"] ||= Hash.new
           d["chefjig"]["client"] = clientinfo
           nr.data = d
           nr.save!
